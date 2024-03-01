209561289d1e12c281036c3d2ba697ef193ba870
fidilen <fidilen.dev@gmail.com>
2024-03-01 09:36:54 +0800

    new interface: insert

--- index.js
+++ index.js
@@ -95,82 +95,90 @@ GSheets.select = async (params) => {
     return data
 }
 
+GSheets.insert = async (params) => {
+    params['insert'] = true
+
+    await GSheets.append(params)
+}
+
 GSheets.append = async (params) => {
-    try {
-        const sheet_id = params.sheet_id
-        const range = params.range
-        const records = params.records
+    const sheet_id = params.sheet_id
+    const range = params.range
+    const records = params.records
+    const insert = params.insert || false
 
-        const rows = await get({ sheet_id, range, render_type: RENDER_TYPE.FORMULA })
+    const rows = await get({ sheet_id, range, render_type: RENDER_TYPE.FORMULA })
 
-        if (rows.length < 1) throw new Error("No header found.")
+    if (rows.length < 1) throw new Error("No header found.")
 
-        const header = rows[0] || []
-        const data = rows
+    const header = rows[0] || []
+    const data = rows
 
-        for (let record of records) {
-            let newRow = []
+    for (let record of records) {
+        let newRow = []
 
-            for (let key of header) {
-                let value = record[key] || ''
+        for (let key of header) {
+            let value = record[key] || ''
 
-                newRow.push(value)
-            }
+            newRow.push(value)
+        }
 
+        let rowIndex = -1
+
+        if (insert) {
+            rowIndex = data.findIndex(row => row.length == 0)
+        }
+
+        if (rowIndex != -1) {
+            data[rowIndex] = newRow
+        } else {
             data.push(newRow)
         }
 
-        console.log(data)
-
-        await update({ sheet_id, range, values: data })
-    } catch (e) {
-        throw new Error(e);
     }
+
+    await update({ sheet_id, range, values: data })
 }
 
 GSheets.update = async (params) => {
-    try {
-        let sheet_id = params.sheet_id
-        let range = params.range
-        let where = params.where || {}
-        let fields = params.fields || {}
+    let sheet_id = params.sheet_id
+    let range = params.range
+    let where = params.where || {}
+    let fields = params.fields || {}
 
-        const rows = await get({ sheet_id, range })
+    const rows = await get({ sheet_id, range })
 
-        if (rows.length < 1) throw new Error("No header found.")
+    if (rows.length < 1) throw new Error("No header found.")
 
-        const header = rows[0] || []
-        const data = rows
-        const values = await get({ sheet_id, range, render_type: RENDER_TYPE.FORMULA })
+    const header = rows[0] || []
+    const data = rows
+    const values = await get({ sheet_id, range, render_type: RENDER_TYPE.FORMULA })
 
-        for (let key of Object.keys(where)) {
-            let columnIndex = header.indexOf(key)
-            let criteria = where[key]
+    for (let key of Object.keys(where)) {
+        let columnIndex = header.indexOf(key)
+        let criteria = where[key]
 
-            if (columnIndex == -1) continue
+        if (columnIndex == -1) continue
 
-            for (let i = 1; i < data.length; i++) {
-                let row = data[i]
+        for (let i = 1; i < data.length; i++) {
+            let row = data[i]
 
-                if (row[columnIndex] == criteria) {
+            if (row[columnIndex] == criteria) {
 
-                    for (let key of Object.keys(fields)) {
-                        let valueIndex = header.indexOf(key)
+                for (let key of Object.keys(fields)) {
+                    let valueIndex = header.indexOf(key)
 
-                        let newRow = row
-                        newRow[valueIndex] = fields[key]
-                        row = newRow
-                    }
-
-                    values[i] = row
+                    let newRow = row
+                    newRow[valueIndex] = fields[key]
+                    row = newRow
                 }
+
+                values[i] = row
             }
         }
-
-        await update({ sheet_id, range, values })
-    } catch (e) {
-        throw new Error(e);
     }
+
+    await update({ sheet_id, range, values })
 }
 
 async function get(params) {
